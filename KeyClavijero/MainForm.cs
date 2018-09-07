75e8fdc6bf1d5ee7a5898511c3d012cfb53c91f7
Yeu Garcia <a11001153@alumno.clavijero.edu.mx>
2018-09-06 21:06:08 -0500

    version1

---
+++ KeyClavijero/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: A11001153
+ * Date: 17-Apr-18
+ * Time: 11:20 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace KeyClavijero
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
---
+++ KeyClavijero/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: A11001153
+ * Date: 17-Apr-18
+ * Time: 11:20 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace KeyClavijero
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			
+			Clipboard.SetText("a18000357");
+			Console.Beep();
+			System.Environment.Exit(1);
+		}
+		
+	}
+}
